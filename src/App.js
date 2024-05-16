229def3f9d528818a4f6ec7a3f10d5e4b84f5f39
rufunt <jazzoo@mail.ru>
2024-05-16 14:29:14 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [value, setValue] = useState(0);
+  const count = Math.round((value / step) * 30.4);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  function handleStepUp() {
+    setStep(step + 1);
+  }
+
+  function handleStepDown() {
+    setStep(step - 1);
+  }
+
+  function handleReset() {
+    setValue(0);
+    setStep(1);
+  }
+
+  return (
+    <>
+      <div>
+        <button onClick={handleStepDown}>-</button>
+        <span>
+          Step:
+          <input
+            type="range"
+            min="0"
+            max="10"
+            value={step}
+            onChange={(e) => setStep(Number(e.target.value))}
+          />
+          {step}
+        </span>
+        <button onClick={handleStepUp}>+</button>
+      </div>
+      <div>
+        <span>Count: </span>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(Number(e.target.value))}
+        />
+      </div>
+      <p>
+        {count} days from today is {date.toDateString()}
+      </p>
+
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
+    </>
+  );
+}
